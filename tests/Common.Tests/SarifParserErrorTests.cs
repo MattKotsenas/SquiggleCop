12ccb9f9d8bbe37cc544b1eb5235ee984a812996
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-07-14 18:15:15 -0700

    Add snapshot test for V2

---
+++ tests/Common.Tests/SarifParserErrorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace SquiggleCop.Common.Tests;
+
+public class SarifParserErrorTests
+{
+    private readonly TestDataReader _reader = new();
+    private readonly SarifParser _parser = new();
+
+    [Fact]
+    public async Task V1LogsThrowSpecificException()
+    {
+        await using Stream data = _reader.Read("Log.v1.sarif");
+
+        await _parser.Invoking(p => p.ParseAsync(data))
+            .Should()
+            .ThrowAsync<InvalidDataException>()
+            .Where(e => e.Message.Contains("SARIF v1"));
+    }
+
+    [Fact]
+    public async Task V2LogsCreatedByAnOldCompilerThrowException()
+    {
+        await using Stream data = _reader.Read("Log.old.v2.sarif");
+
+        await _parser.Invoking(p => p.ParseAsync(data))
+            .Should()
+            .ThrowAsync<InvalidDataException>()
+            .Where(e => e.Message.Contains("SDK 8.0.100 or later"));
+    }
+}
--- tests/Common.Tests/SarifParserTests.cs
+++ tests/Common.Tests/SarifParserTests.cs
@@ -1,4 +1,4 @@
-﻿namespace SquiggleCop.Common.Tests;
+namespace SquiggleCop.Common.Tests;
 
 public class SarifParserTests
 {
@@ -6,24 +6,12 @@ public class SarifParserTests
     private readonly SarifParser _parser = new();
 
     [Fact]
-    public async Task V1LogsThrowSpecificException()
+    public async Task SnapshotTest()
     {
-        await using Stream data = _reader.Read("Log.v1.sarif");
+        await using Stream data = _reader.Read("Log.v2.sarif");
 
-        await _parser.Invoking(p => p.ParseAsync(data))
-            .Should()
-            .ThrowAsync<InvalidDataException>()
-            .Where(e => e.Message.Contains("SARIF v1"));
-    }
+        IReadOnlyCollection<DiagnosticConfig> configs = await _parser.ParseAsync(data);
 
-    [Fact]
-    public async Task V2LogsCreatedByAnOldCompilerThrowException()
-    {
-        await using Stream data = _reader.Read("Log.old.v2.sarif");
-
-        await _parser.Invoking(p => p.ParseAsync(data))
-            .Should()
-            .ThrowAsync<InvalidDataException>()
-            .Where(e => e.Message.Contains("SDK 8.0.100 or later"));
+        await Verify(configs);
     }
 }
