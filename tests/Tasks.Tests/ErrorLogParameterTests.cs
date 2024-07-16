2ab7b8a0397f64501f04ad4aad30e340fd45f546
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-07-15 22:53:33 -0700

    Add test stubs for MSBuild task

---
+++ tests/Tasks.Tests/ErrorLogParameterTests.cs
@@ -0,0 +1,31 @@
+namespace SquiggleCop.Common.Tests;
+
+public class ErrorLogParameterTests
+{
+    // TODO: Use MSBuildProjectCreator with a custom implementation of CoreCompile (or similar)
+    // to fake the compilation and set an output file.
+
+    [Fact]
+    public void NoErrorLogReportsWarning()
+    {
+        // TODO: Implement
+    }
+
+    [Fact]
+    public void MissingErrorLogReportsWarning()
+    {
+        // TODO: Implement
+    }
+
+    [Fact]
+    public void V1ErrorLogReportsWarning()
+    {
+        // TODO: Implement
+    }
+
+    [Fact]
+    public void V2ErrorLogNoWarning()
+    {
+        // TODO: Implement
+    }
+}
---
+++ tests/Tasks.Tests/SquiggleBaselineFileTests.cs
@@ -0,0 +1,30 @@
+﻿namespace SquiggleCop.Common.Tests;
+
+public class SquiggleBaselineFileTests
+{
+    // TODO: Use MSBuildProjectCreator with a custom implementation of CoreCompile (or similar)
+    // to fake the compilation and set an output file.
+
+    [Theory]
+    [CombinatorialData]
+    public void NoBaselineFile(bool automaticBaseline, bool explicitFile)
+    {
+        // TODO: Implement; when auto is on, create the file
+        // TODO: Implement; when auto is off, do what? Give message to run tool?
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public void BaselineUpToDate(bool automaticBaseline, bool explicitFile)
+    {
+        // TODO: Implement; in both cases, ensure to not set the modified date
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public void BaselineOutOfDate(bool automaticBaseline, bool explicitFile)
+    {
+        // TODO: Implement; when auto is on, update the file
+        // TODO: Implement; when auto is off, pop Verify? Give message to run tool?
+    }
+}
