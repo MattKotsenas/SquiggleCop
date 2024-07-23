3cf03c511d0ceab7182b5a2a5b7c77539f80d1c6
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-07-23 15:08:03 -0700

    Add usage CLI test

---
+++ tests/Tool.Tests/CommandLineTests.cs
@@ -0,0 +1,31 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+namespace Tool.Tests;
+
+
+public class CommandLineTests : TestBase
+{
+    private async Task<BufferedCommandResult> Install(string temp, string nugetConfig)
+    {
+        // Add our temp directory to %PATH% so installed tools can be found and executed
+        Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH") + $"{Path.PathSeparator}{TestRootPath}");
+
+        string[] args = $"tool install squigglecop.tool --tool-path {temp} --prerelease --configfile {nugetConfig}".Split(" ");
+
+        return await Cli.Wrap("dotnet")
+        .WithArguments(args)
+        .ExecuteBufferedAsync()
+        .ConfigureAwait(false);
+    }
+
+    [Fact]
+    public async Task PrintUsage()
+    {
+        await Install(TestRootPath, Repository.NuGetConfigPath);
+
+        BufferedCommandResult result = await Cli.Wrap("dotnet-squigglecop").ExecuteBufferedAsync();
+
+        await Verify(result).IgnoreMembers(typeof(CommandResult), "StartTime", "ExitTime", "RunTime");
+    }
+}
---
+++ tests/Tool.Tests/TestBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+using Microsoft.Build.Utilities.ProjectCreation;
+
+namespace Tool.Tests;
+
+public abstract class TestBase : IDisposable
+{
+    protected TestBase()
+    {
+        TestRootPath = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName())).FullName;
+
+        Uri feed = new(new FileInfo(Assembly.GetExecutingAssembly().Location).Directory!.FullName);
+
+        Repository = PackageRepository.Create(TestRootPath, /*new Uri("https://api.nuget.org/v3/index.json"),*/ feed);
+    }
+
+    protected string TestRootPath { get; }
+    protected PackageRepository Repository { get; }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    [SuppressMessage("Roslynator", "RCS1075:Avoid empty catch clause that catches System.Exception", Justification = "Test cleanup that can be impacted by other factors like AV software.")]
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            Repository.Dispose();
+        }
+
+        if (Directory.Exists(TestRootPath))
+        {
+            try
+            {
+                Directory.Delete(TestRootPath, recursive: true);
+            }
+            catch (Exception)
+            {
+                // Ignored
+            }
+        }
+    }
+}
